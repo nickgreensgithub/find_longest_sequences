8bd72a2edaaac2508be327cf1617b51cf5d7d13a
Nicholas Green <nickgreenweb@gmail.com>
2022-02-02 15:57:31 +0100

    multithreaded with pointers

--- main.cpp
+++ main.cpp
@@ -46,13 +46,15 @@ void eraseWhereLongerSequenceExists(vector<fastaEntry> records);
 vector<fastaEntry> filterRecords(vector<fastaEntry> records);
 //void filterRecords(vector<fastaEntry> records);
 void WriteToFastaFile(string path, vector<fastaEntry> records);
-vector<vector<fastaEntry>> SplitVector(vector<fastaEntry> records, int n = 10);
+vector<vector<fastaEntry*>> SplitVector(vector<fastaEntry>& records, int n = 10);
+
+vector<fastaEntry*> GetPointersForRange(vector<fastaEntry>::iterator& startItem, long count);
 
 int main(int argc, char* argv[])
 {
     auto start = chrono::high_resolution_clock::now();
-    //auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/top10k.fa");
-    auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/test_dataset.fa");
+    auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/top10k.fa");
+    //auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/test_dataset.fa");
     filterRecords(records);
     WriteToFastaFile("/mnt/files/projects/aau/sequence_matcher/output_dataset.fa", records);
     auto stop = chrono::high_resolution_clock::now();
@@ -61,9 +63,9 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-static bool DoesLongerSequenceExist(const fastaEntry thisRecord, vector<fastaEntry> records){
+static bool DoesLongerSequenceExist(const fastaEntry* thisRecord, vector<fastaEntry> records){
     for(auto record : records){
-        if (thisRecord.IsShorterVersionOf(record)){
+        if (thisRecord->IsShorterVersionOf(record)){
             return true;
         }
     }
@@ -89,71 +91,47 @@ static bool DoesLongerSequenceExist(const fastaEntry thisRecord, vector<fastaEnt
 //    return chunks;
 //}
 
-vector<vector<fastaEntry>> SplitVector(vector<fastaEntry> records, int n){
-    vector<vector<fastaEntry>> chunks{};
+//vector<fastaEntry> GetPointersForRange(){
+//
+//}
+
+vector<fastaEntry*> GetPointersForRange(vector<fastaEntry>::iterator& startItem, long count) {
+    auto endItem =  startItem+ count;
+    vector<fastaEntry *> result;
+    for (auto begin = startItem, end = endItem; begin != end; ++begin){
+        result.push_back(&(*begin));
+    }
+    return result;
+}
+
+vector<vector<fastaEntry*>> SplitVector(vector<fastaEntry>& records, int n) {
+    vector<vector<fastaEntry *>> chunks{};
     auto fullSize = records.size();
     auto itr = records.begin();
     for (int k = 0; k < n; ++k) {
-        auto chunkSize = fullSize / (n - k);
+        long chunkSize = fullSize / (n - k);
         fullSize -= chunkSize;
-        //TODO make the chunks use pointers to the original fastaEntry objects instead of copying them,should reduce ram overhead
-        chunks.emplace_back(vector<fastaEntry>{itr, itr+chunkSize});
+        chunks.push_back(GetPointersForRange(itr, chunkSize));
         itr += chunkSize;
     }
     return chunks;
-
-
-//    // determine the total number of sub-vectors of size `n`
-//    int size = (records.size() - 1) / n + 1;
-//
-//    // create an array of vectors to store the sub-vectors
-//    std::vector<fastaEntry> vec[size];
-//
-//    // each iteration of this loop process the next set of `n` elements
-//    // and store it in a vector at k'th index in `vec`
-//    for (int k = 0; k < size; ++k)
-//    {
-//    // get range for the next set of `n` elements
-//    auto start_itr = std::next(records.cbegin(), k*n);
-//    auto end_itr = std::next(records.cbegin(), k*n + n);
-//
-//    // allocate memory for the sub-vector
-//    vec[k].resize(n);
-//
-//    // code to handle the last sub-vector as it might
-//    // contain fewer elements
-//    if (k*n + n > records.size())
-//    {
-//    end_itr = records.cend();
-//    vec[k].resize(records.size() - k*n);
-//    }
-//
-//    // copy elements from the input range to the sub-vector
-//    copy(start_itr, end_itr, vec[k].begin());
-//
-//    return vec;
 }
-
-//void filterRecords(vector<fastaEntry> records){
-//    records.erase( std::remove_if( records.begin(), records.end(), [&records](fastaEntry& record){
-//            return DoesLongerSequenceExist(record, records);
-//        } ), records.end());
-//}
-
 
 vector<fastaEntry> filterRecords(vector<fastaEntry> records){
     auto chunkedInput = SplitVector(records);
 
     #pragma omp parallel for
     for(auto chunk : chunkedInput) {
-        chunk.erase( std::remove_if( chunk.begin(), chunk.end(), [&records](fastaEntry& record){
+        chunk.erase( std::remove_if( chunk.begin(), chunk.end(), [&records](fastaEntry* record){
             return DoesLongerSequenceExist(record, records);
         } ), chunk.end());
     }
 
     vector<fastaEntry> result;
     for(auto chunk : chunkedInput) {
-        copy(chunk.begin(), chunk.end(), std::back_inserter(result));
+        for(auto record: chunk){
+            result.push_back(*record);
+        }
     }
     return result;
 }
