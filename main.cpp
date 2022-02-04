7160efb0abf244a2bf40e737299c7c18f7915295
Nicholas Green <nickgreenweb@gmail.com>
2022-02-04 15:55:18 +0100

    Added input parameters

--- main.cpp
+++ main.cpp
@@ -37,37 +37,65 @@ struct fastaEntry{
         return false;
     }
 };
-
+int THREADS;
 vector<fastaEntry> ReadFromFastaFile(string path);
 void WriteToFastaFile(string path, vector<fastaEntry> records);
 void eraseWhereLongerSequenceExists(vector<fastaEntry> records);
 vector<fastaEntry> filterRecords(vector<fastaEntry> records);
 //void filterRecords(vector<fastaEntry> records);
 void WriteToFastaFile(string path, vector<fastaEntry> records);
-vector<vector<fastaEntry*>> SplitVector(vector<fastaEntry>& records, int n = 10);
+vector<vector<fastaEntry*>> SplitVector(vector<fastaEntry>& records, int n = THREADS);
 
 vector<fastaEntry*> GetPointersForRange(vector<fastaEntry>::iterator& startItem, long count);
+int CheckArgumentCount(int argc, char* argv[]);
+
 
 int main(int argc, char* argv[])
 {
+    auto countStatus = CheckArgumentCount(argc, argv);
+    if(countStatus > 0){
+        return countStatus;
+    }
+    if(argc >= 4){
+        int threads = stoi(argv[3]);
+        THREADS = (threads > 0) ? threads : 1;
+    }
+
+    auto inputPath = argv[1]; //TODO check if exists
+    auto outputPath = argv[1];
+
+
+
     auto start = chrono::high_resolution_clock::now();
-
-        //auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/top10k.fa");
-        auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/20seqs.fa");
-        //auto records = ReadFromFastaFile("/mnt/files/projects/aau/sequence_matcher/test.fa");
-        cout << "original count: "<< records.size() << endl;
+    auto records = ReadFromFastaFile(inputPath);
+    cout << "original count: "<< records.size() << endl;
 
 
-        auto filtered = filterRecords(records);
-        cout << "reduced count: "<< filtered.size() << endl;
-        //WriteToFastaFile("/mnt/files/projects/aau/sequence_matcher/output_dataset.fa", filtered);
-        WriteToFastaFile("/mnt/files/projects/aau/sequence_matcher/Nick_10k_filtered.fa", filtered);
+    auto filtered = filterRecords(records);
+    cout << "reduced count: "<< filtered.size() << endl;
+    WriteToFastaFile(outputPath, filtered);
 
     auto stop = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
     cout << "Process took: " << duration.count() << " seconds to complete" <<endl;
 
     return 0;
+}
+
+int CheckArgumentCount(int argc, char* argv[]){
+    int result = 0;
+    if (argc < 2) {
+        std::cerr << "missing input file" << endl;
+        result =  EXIT_FAILURE;
+    }
+    if (argc < 3) {
+        std::cerr << "missing output file" << endl;
+        result =  EXIT_FAILURE;
+    }
+    if(result > 0){
+        std::cerr << "usage: longest_sequence <input_file> <output_file> <threads>";
+    }
+    return result;
 }
 
 static bool DoesLongerSequenceExist(const fastaEntry* thisRecord, vector<fastaEntry>* records){
@@ -104,7 +132,7 @@ vector<vector<fastaEntry*>> SplitVector(vector<fastaEntry>& records, int n) {
 vector<fastaEntry> filterRecords(vector<fastaEntry> records){
     auto chunkedInput = SplitVector(records);
 
-    #pragma omp parallel for
+    #pragma omp parallel for num_threads(THREADS)
     for(auto& chunk : chunkedInput) {
         chunk.erase(remove_if(chunk.begin(), chunk.end(), [&records](const fastaEntry* record){
             return DoesLongerSequenceExist(record, &records);
